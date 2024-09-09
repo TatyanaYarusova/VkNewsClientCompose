e89f4890fb514f3c8519d34486d9ee039fbaf7d3
Tatyana Yarusova <tata_yarusova@mail.ru>
2024-09-09 11:51:15 +0300

    add lazy column and swipe to dismiss

--- app/src/main/java/com/example/vknewsclientapp/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/vknewsclientapp/presentation/MainViewModel.kt
@@ -6,23 +6,44 @@ import androidx.lifecycle.ViewModel
 import com.example.vknewsclientapp.domain.FeedPost
 import com.example.vknewsclientapp.domain.StatisticItem
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
 
-    private val _feedPost = MutableLiveData(FeedPost())
-    val feedPost: LiveData<FeedPost> = _feedPost
+    private val sourceList = mutableListOf<FeedPost>().apply {
+        repeat(10) {
+            add(FeedPost(id = it))
+        }
+    }
+    private val _feedPosts = MutableLiveData<List<FeedPost>>(sourceList)
+    val feedPosts: LiveData<List<FeedPost>> = _feedPosts
 
-    fun updateCount(item: StatisticItem) {
-        val oldStatistic = feedPost.value?.statistic ?: throw IllegalStateException()
+    fun updateCount(feedPost: FeedPost, item: StatisticItem) {
+        val oldPosts = feedPosts.value?.toMutableList() ?: mutableListOf()
+        val oldStatistic = feedPost.statistic
         val newStatistics = oldStatistic.toMutableList().apply {
             replaceAll { oldItem ->
-                if (oldItem.type == item.type){
+                if (oldItem.type == item.type) {
                     oldItem.copy(count = oldItem.count + 1)
                 } else {
                     oldItem
                 }
             }
         }
-        _feedPost.value = feedPost.value?.copy(statistic = newStatistics)
+        val newFeedPost = feedPost.copy(statistic = newStatistics)
+        _feedPosts.value = oldPosts.apply {
+            replaceAll {
+                if (it.id == newFeedPost.id) {
+                    newFeedPost
+                } else {
+                    it
+                }
+            }
+        }
+    }
+
+    fun remove(item: FeedPost) {
+        val oldPosts = feedPosts.value?.toMutableList() ?: mutableListOf()
+        oldPosts.remove(item)
+        _feedPosts.value = oldPosts
     }
 
 }
--- app/src/main/java/com/example/vknewsclientapp/ui/VkNewsMainScreen.kt
+++ app/src/main/java/com/example/vknewsclientapp/ui/VkNewsMainScreen.kt
@@ -1,14 +1,24 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
 package com.example.vknewsclientapp.ui
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableIntStateOf
@@ -16,10 +26,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.example.vknewsclientapp.domain.FeedPost
 import com.example.vknewsclientapp.presentation.MainViewModel
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen(
@@ -62,14 +72,48 @@ fun MainScreen(
 
         }
     ) {
-        val feedPost = viewModel.feedPost.observeAsState(FeedPost())
-        PostCard(
-            modifier = Modifier.padding(all = 8.dp),
-            feedPost = feedPost.value,
-            onViewsClickListener = viewModel::updateCount,
-            onLikeClickListener =viewModel::updateCount,
-            onShareClickListener =viewModel::updateCount,
-            onCommentClickListener =viewModel::updateCount
-        )
+        val feedPosts = viewModel.feedPosts.observeAsState(listOf())
+        LazyColumn(
+            contentPadding = PaddingValues(
+                top = 8.dp,
+                start = 8.dp,
+                end = 8.dp,
+                bottom = 72.dp
+            ),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(
+                items = feedPosts.value,
+                key = { it.id }
+            ) { post ->
+                val dismissState = rememberSwipeToDismissBoxState()
+                if(dismissState.currentValue == SwipeToDismissBoxValue.EndToStart) {
+                    viewModel.remove(post)
+                }
+
+                SwipeToDismissBox(
+                    modifier = Modifier.animateItemPlacement(),
+                    enableDismissFromStartToEnd = true,
+                    state = dismissState,
+                    backgroundContent = {}
+                ){
+                    PostCard(
+                        feedPost = post,
+                        onViewsClickListener = { statisticItem ->
+                            viewModel.updateCount(post, statisticItem)
+                        },
+                        onLikeClickListener = { statisticItem ->
+                            viewModel.updateCount(post, statisticItem)
+                        },
+                        onShareClickListener = { statisticItem ->
+                            viewModel.updateCount(post, statisticItem)
+                        },
+                        onCommentClickListener = { statisticItem ->
+                            viewModel.updateCount(post, statisticItem)
+                        }
+                    )
+                }
+            }
+        }
     }
 }
